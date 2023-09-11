5d5e0c9133c0bc5a70292f56ad13ab2af319c08f
lmf <lmf20030510@163.com>
2023-09-11 16:55:34 +0800

    null

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -6,7 +6,6 @@ import com.itheima.reggie.entity.User;
 import com.itheima.reggie.service.UserService;
 import com.itheima.reggie.utils.MailUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,12 +35,12 @@ public class UserController {
     @PostMapping("/sendMsg")
     public Result<String> sendMsg(@RequestBody User user, HttpSession session) throws MessagingException {
         String phone = user.getPhone();
-        if (StringUtils.isNotEmpty(phone)) {
+        if (!phone .isEmpty()) {
             //随机生成一个验证码
             String code = MailUtils.achieveCode();
             log.info("code={}", code);
             //这里的phone其实就是邮箱，code是我们生成的验证码
-            MailUtils.sendTestMail(phone, code);
+            //MailUtils.sendTestMail(phone, code);
             //验证码存session，方便后面拿出来比对
             session.setAttribute(phone, code);
             return Result.success("验证码发送成功");
@@ -65,7 +64,7 @@ public class UserController {
         //从session中获取验证码
         String codeInSession = session.getAttribute(phone).toString();
         //比较这用户输入的验证码和session中存的验证码是否一致
-        if (codeInSession != null && codeInSession.equals(code)) {
+        if (code != null && code.equals(codeInSession)) {
             //如果输入正确，判断一下当前用户是否存在
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             //判断依据是从数据库中查询是否有其邮箱
